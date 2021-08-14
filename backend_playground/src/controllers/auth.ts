52f0f8f8eca4ec3fe8317e7ec5a797406e72e3d9
AkashSDas <aakashdas368@gmail.com>
2021-08-14 19:23:22 +0530

    sign up user in auth.ts inside router/

--- backend_playground/src/controllers/auth.ts
+++ backend_playground/src/controllers/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 
+import { User } from "../models/user";
+
 export function signout(req: Request, res: Response) {
   res.json({
     message: "sign out",
@@ -7,9 +9,16 @@ export function signout(req: Request, res: Response) {
 }
 
 export function signup(req: Request, res: Response) {
-  console.log(req.body);
-
-  res.json({
-    message: "sign up",
+  const user = new User(req.body);
+  user.save((err, user) => {
+    return err
+      ? res.status(400).json({
+          error: "Not able to save user in database",
+        })
+      : res.json({
+          name: user.username,
+          email: user.email,
+          id: user._id,
+        });
   });
 }
