b8404c096f54284029531a54578b25223f67012d
AkashSDas <aakashdas368@gmail.com>
2021-08-14 18:58:30 +0530

    refactor api.ts and use authRouter

--- backend_playground/src/api.ts
+++ backend_playground/src/api.ts
@@ -1,6 +1,8 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
+
+import { router as authRouter } from "./routes/auth";
 
 // App
 export const app = express();
@@ -10,22 +12,27 @@ export const app = express();
 app.use(cookieParser());
 app.use(cors());
 
+// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4#:~:text=Explanation%3A%20The%20default%20value%20of,%2Dof%2Dthe%2Dbox.
+app.use(express.json());
+
 ////// APIs //////
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("hello world");
-});
+// app.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("hello world");
+// });
 
-app.get("/about", (req: Request, res: Response, next: NextFunction) => {
-  res.send("about us");
-});
+// app.get("/about", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("about us");
+// });
 
-// Using middleware
-const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  console.log("Authenticating!");
-  next();
-};
+// // Using middleware
+// const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+//   console.log("Authenticating!");
+//   next();
+// };
 
-app.get("/dashboard", isAuthenticated, (req: Request, res: Response) => {
-  res.send("Dashboard");
-});
+// app.get("/dashboard", isAuthenticated, (req: Request, res: Response) => {
+//   res.send("Dashboard");
+// });
+
+app.use("/api/auth", authRouter);
