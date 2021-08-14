b40ed70d89fce397c7bd3d1afc8bbfde6c1f80f3
AkashSDas <aakashdas368@gmail.com>
2021-08-14 20:00:08 +0530

    add validations for signup router in auth.ts

--- backend_playground/src/routes/auth.ts
+++ backend_playground/src/routes/auth.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { check } from "express-validator";
 
 import { signout, signup } from "../controllers/auth";
 
 export const router = Router();
 
 router.get("/signout", signout);
-router.post("/signup", signup);
+
+const signUpValidations = [
+  check("username", "Username should be atleast 2 characters").isLength({
+    min: 2,
+  }),
+  check("email", "Email is required").isEmail(),
+  check("password", "Password should be atleast 6 characters").isLength({
+    min: 6,
+  }),
+];
+router.post("/signup", signUpValidations, signup);
