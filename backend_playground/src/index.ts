7ae24e7a56124fc42141e2b8cba0d42a66bb8b69
AkashSDas <aakashdas368@gmail.com>
2021-08-14 17:46:44 +0530

    load env variables in src/index.ts and connect to mongodb atlas

--- backend_playground/src/index.ts
+++ backend_playground/src/index.ts
@@ -1,4 +1,19 @@
+import { config } from "dotenv";
+import { connect } from "mongoose";
+
 import { app } from "./api";
+
+// Environment Variables (Stripe API Key)
+if (process.env.NODE_ENV !== "production") {
+  config();
+}
+
+// Connecting to MongoDB
+connect(process.env.MONGODB_CONNECT_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => console.log("Connect to MongoDB Atlas"));
 
 // Start the server
 const port = process.env.PORT || 8000;
