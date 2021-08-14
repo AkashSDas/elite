7fb9b243d2a3a4f4f7d09df0e14a3f3456b1a3a5
AkashSDas <aakashdas368@gmail.com>
2021-08-14 18:08:26 +0530

    using middlewares in api.ts

--- backend_playground/src/api.ts
+++ backend_playground/src/api.ts
@@ -1,7 +1,14 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 
 // App
 export const app = express();
+
+// Middlewares
+
+app.use(cookieParser());
+app.use(cors());
 
 ////// APIs //////
 
@@ -11,4 +18,14 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 
 app.get("/about", (req: Request, res: Response, next: NextFunction) => {
   res.send("about us");
+});
+
+// Using middleware
+const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Authenticating!");
+  next();
+};
+
+app.get("/dashboard", isAuthenticated, (req: Request, res: Response) => {
+  res.send("Dashboard");
 });
