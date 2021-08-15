83c80247d33fbc380faf723e666de314b256e370
AkashSDas <aakashdas368@gmail.com>
2021-08-15 14:34:06 +0530

    create custom middlewares in auth.ts in controllers/

--- backend_playground/src/controllers/auth.ts
+++ backend_playground/src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "express-jwt";
 import { validationResult } from "express-validator";
 import * as jsonwebtoken from "jsonwebtoken";
@@ -78,3 +78,52 @@ export const isSignedIn = jwt({
   userProperty: "auth",
   algorithms: ["HS256"],
 });
+
+// Custom middleware
+
+// Extending he Request type to have custom properties like
+// profile, auth, etc...
+// https://stackoverflow.com/questions/44383387/typescript-error-property-user-does-not-exist-on-type-request
+interface IIsAuthenticatedRequest extends Request {
+  user: any;
+  profile: any;
+  auth: any;
+}
+
+export function isAuthenticated(
+  req: IIsAuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const profile = req.profile; // from front end
+  const auth = req.auth; // this property is set on isSignedIn middleware (define above this middleware)
+  const sameUser = req.profile._id == req.auth._id;
+  const check = profile && auth && sameUser;
+
+  if (!check) {
+    return res.status(403).json({
+      error: "Access denied",
+    });
+  }
+
+  next();
+}
+
+interface IIsAdminRequest extends Request {
+  profile: any;
+}
+
+export function isAdmin(
+  req: IIsAdminRequest,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.profile.role === 0) {
+    // Regular user
+    return res.status(403).json({
+      error: "Your not admin, Access denied",
+    });
+  }
+
+  next();
+}
