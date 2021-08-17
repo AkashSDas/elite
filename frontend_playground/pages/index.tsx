aea77f1d97e4876fc2c093a710cc51fadcd1c282
AkashSDas <aakashdas368@gmail.com>
2021-08-17 16:52:42 +0530

    use base layout component in home page

--- frontend_playground/pages/index.tsx
+++ frontend_playground/pages/index.tsx
@@ -1,10 +1,21 @@
+import BaseLayout from "../components/base_layout";
 import styles from "../styles/Home.module.css";
 
 function Index() {
   return (
-    <main>
-      <h1>Index Page</h1>
-    </main>
+    <BaseLayout title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </BaseLayout>
   );
 }
 
