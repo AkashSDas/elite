100d13323c3aa79837f69cd0b86289eb9defee42
AkashSDas <aakashdas368@gmail.com>
2021-08-17 08:35:15 +0530

    in product route change the param from category to productId

--- backend_playground/src/routes/product.ts
+++ backend_playground/src/routes/product.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
-import { getCategoryById } from "../controllers/category";
-import { createProduct } from "../controllers/product";
+import { createProduct, getProductById } from "../controllers/product";
 import { getUserById } from "../controllers/user";
 import { isAdmin, isAuthenticated, isSignedIn } from "../controllers/auth";
 
@@ -8,7 +7,7 @@ export const router = Router();
 
 // params
 router.param("userId", getUserById);
-router.param("category", getCategoryById);
+router.param("productId", getProductById);
 
 // routes
 
