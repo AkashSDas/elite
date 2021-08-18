78736df19a7d5ec7081a66ef4db0c3bbef02d14a
AkashSDas <aakashdas368@gmail.com>
2021-08-18 09:44:28 +0530

    add conditional rendering based on auth state in navbar

--- frontend_playground/components/navbar.tsx
+++ frontend_playground/components/navbar.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
+import { MouseEventHandler } from "react";
+import { isAuthenticated, signout } from "../lib/auth";
 
 function Navbar() {
+  const router = useRouter();
+
   return (
     <div>
       <ul className="nav nav-tabs bg-dark">
@@ -9,19 +13,40 @@ function Navbar() {
         <NavItem href="/cart" text="Cart" />
         <NavItem href="/dashboard/user" text="Dashboard" />
         <NavItem href="/dashboard/admin" text="A Dashboard" />
-        <NavItem href="/signup" text="Sign up" />
-        <NavItem href="/signin" text="Sign in" />
-        <NavItem href="/signout" text="Signout" />
+        {!isAuthenticated() && <NavItem href="/signup" text="Sign up" />}
+        {!isAuthenticated() && <NavItem href="/signin" text="Sign in" />}
+        {isAuthenticated() && (
+          <li
+            className="nav-item"
+            onClick={() =>
+              signout(() => {
+                router.push("/");
+              })
+            }
+          >
+            <a className="nav-link" style={{ color: "grey" }}>
+              Signout
+            </a>
+          </li>
+        )}
       </ul>
     </div>
   );
 }
 
-function NavItem({ href, text }: { href: string; text: string }) {
+function NavItem({
+  href,
+  text,
+  onClick,
+}: {
+  href: string;
+  text: string;
+  onClick?: MouseEventHandler<HTMLLIElement>;
+}) {
   const router = useRouter();
 
   return (
-    <li className="nav-item">
+    <li className="nav-item" onClick={onClick}>
       <Link href={href}>
         <a
           className="nav-link"
