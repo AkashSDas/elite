308613511d9f00eef4b36f3b8280c8da44f40462
AkashSDas <aakashdas368@gmail.com>
2021-08-19 21:47:09 +0530

    initialize and create make payment using stripe function

---
+++ backend_playground/src/controllers/stripe_payment.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import * as uuid from "uuid";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: "2020-08-27",
+});
+
+export async function makePayment(req: Request, res: Response) {
+  const { token, products } = req.body;
+
+  let amount = 0;
+  products.map((product) => (amount = amount + product.price));
+
+  const idempotencyKey = uuid();
+
+  const customer = await stripe.customers.create({
+    email: token.email,
+    source: token.id,
+  });
+
+  if (customer) {
+    try {
+      const result = await stripe.charges.create(
+        {
+          amount,
+          currency: "inr",
+          customer: customer.id,
+          receipt_email: token.email,
+        },
+        { idempotencyKey }
+      );
+      return res.status(200).json(result);
+    } catch (err) {
+      res.status(400).json({ error: "Cannot charge" });
+    }
+  }
+}
