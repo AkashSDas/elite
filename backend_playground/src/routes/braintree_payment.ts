26df63ea20bd3ac4a16ecb75372a8098d5c9a8e5
AkashSDas <aakashdas368@gmail.com>
2021-08-20 10:56:57 +0530

    add userId param in router to get user

--- backend_playground/src/routes/braintree_payment.ts
+++ backend_playground/src/routes/braintree_payment.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth";
 import { getToken, makePayment } from "../controllers/braintree_payment";
+import { getUserById } from "../controllers/user";
 
 export const router = Router();
+
+router.param("userId", getUserById);
 
 router.get("/gettoken/:userId", isSignedIn, isAuthenticated, getToken);
 
