d0e703c016d06ba497f07fc26c4beb48006755e9
AkashSDas <aakashdas368@gmail.com>
2021-08-21 18:27:29 +0530

    load env vars and initialize mongodb atlas connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,22 @@
+// First loading env variables and then importing from other modules
+import { config } from "dotenv";
+
+// Load env variables
+if (process.env.NODE_ENV !== "production") config();
+
+import { connect } from "mongoose";
 import { app } from "./api";
+
+// Connect to MongoDB
+connect(process.env.MONGODB_CONNECT_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) =>
+    console.log(`Cannot connect to MongoDB Atlas\nError: ${err}`)
+  );
 
 // Start the server
 const port = process.env.PORT || 8000;
