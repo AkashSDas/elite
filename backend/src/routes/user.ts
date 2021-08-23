556b707be2e8df86a80483db9639633d2d9252c1
AkashSDas <aakashdas368@gmail.com>
2021-08-23 20:08:57 +0530

    add auth check in all users route and create update user route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import getAllUsers from "../controllers/user/get_all_users";
+import updateUser from "../controllers/user/update_user";
 
 export const router = Router();
 
-router.get("/all-users", getAllUsers);
+router.get("/all-users", isSignedIn, isAuthenticated, getAllUsers);
+router.put("/", isSignedIn, isAuthenticated, updateUser);
