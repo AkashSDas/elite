6987213d2bbc077ac5045a529a6fde92d519e9fa
AkashSDas <aakashdas368@gmail.com>
2021-08-23 20:13:51 +0530

    add userId param to user router and change get all user and update user routes

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import getAllUsers from "../controllers/user/get_all_users";
+import getUserById from "../controllers/user/get_user_by_id";
 import updateUser from "../controllers/user/update_user";
 
 export const router = Router();
 
-router.get("/all-users", isSignedIn, isAuthenticated, getAllUsers);
-router.put("/", isSignedIn, isAuthenticated, updateUser);
+router.param("userId", getUserById);
+
+router.get("/:userId/all-users", isSignedIn, isAuthenticated, getAllUsers);
+router.put("/:userId", isSignedIn, isAuthenticated, updateUser);
