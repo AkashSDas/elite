945de4a6c7514395a10f0d1f73f6e59853091e98
AkashSDas <aakashdas368@gmail.com>
2021-08-24 11:03:29 +0530

    create get product by id middleware

---
+++ backend/src/controllers/product/middlewares.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import Product, { ProductDocument } from "../../models/product";
+import { runAsync } from "../../utils";
+import { responseMsg } from "../json_response";
+
+export async function getProductById(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  id: string
+) {
+  const [data, err] = await runAsync(
+    Product.findById(id).populate("category").exec()
+  );
+
+  if (err || !data)
+    return responseMsg(res, {
+      status: 400,
+      message: "Product not found",
+    });
+
+  const product: ProductDocument = data;
+  req.product = product;
+  next();
+}
