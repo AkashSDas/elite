17d63ce59f4bd2b60024bccaef6e596bc42f8496
AkashSDas <aakashdas368@gmail.com>
2021-08-24 10:51:06 +0530

    create product model

---
+++ backend/src/models/product.ts
@@ -0,0 +1,27 @@
+import { Document, model, Schema } from "mongoose";
+import MongoPaging from "mongo-cursor-pagination";
+
+export type ProductDocument = Document & {
+  name: string;
+  description: string;
+  photo: any;
+  category: any;
+  stock: number;
+  sold: number;
+  price: number;
+};
+
+const productSchema = new Schema<ProductDocument>({
+  name: { type: String, trim: true, required: true, maxlength: 32 },
+  description: { type: String, trim: true, required: true, maxlength: 2048 },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  sold: { type: Number, default: 0 },
+  photo: { data: Buffer, contentType: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+});
+
+productSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateProduct" });
+
+const Product = model<ProductDocument>("Product", productSchema);
+export default Product;
