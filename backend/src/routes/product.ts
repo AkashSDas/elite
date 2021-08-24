6dd96292ec6bcdcf08cc1a813803196a1206c5b6
AkashSDas <aakashdas368@gmail.com>
2021-08-24 22:53:44 +0530

    add routes for update and delete and param of productId in product router

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,12 +1,23 @@
 import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import createProduct from "../controllers/product/create";
+import deleteProduct from "../controllers/product/delete";
+import { getProductById } from "../controllers/product/middlewares";
+import updateProduct from "../controllers/product/update";
 import getUserById from "../controllers/user/get_user_by_id";
 
 export const router = Router();
 
 /// Params
 router.param("userId", getUserById);
+router.param("productId", getProductById);
 
 /// Routes
 router.post("/:userId", isSignedIn, isAuthenticated, createProduct);
+router.put("/:productId/:userId", isSignedIn, isAuthenticated, updateProduct);
+router.delete(
+  "/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  deleteProduct
+);
