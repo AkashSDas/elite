30448f6c514cdf45220b5213d7b35c4d74ffa7c8
AkashSDas <aakashdas368@gmail.com>
2021-08-25 18:08:11 +0530

    add orderId param and get order route to order router

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import createOrder from "../controllers/order/create";
+import getOrder from "../controllers/order/get_order";
+import { getOrderById } from "../controllers/order/middlewares";
 import getUserById from "../controllers/user/get_user_by_id";
 
 export const router = Router();
 
 /// Params
 router.param("userId", getUserById);
+router.param("orderId", getOrderById);
 
 /// Routes
 router.post("/:userId", isSignedIn, isAuthenticated, createOrder);
+router.get("/:userId/:orderId", isSignedIn, isAuthenticated, getOrder);
