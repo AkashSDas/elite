b16903f7b17020219b54c4ec24ebf7cf50268b34
AkashSDas <aakashdas368@gmail.com>
2021-08-25 08:49:42 +0530

    change product schema and update product interface

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -4,22 +4,27 @@ import MongoPaging from "mongo-cursor-pagination";
 export type ProductDocument = Document & {
   name: string;
   description: string;
-  photo: any;
+  photoURL: string;
   category: any;
   stock: number;
   sold: number;
   price: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
-const productSchema = new Schema<ProductDocument>({
-  name: { type: String, trim: true, required: true, maxlength: 32 },
-  description: { type: String, trim: true, required: true, maxlength: 2048 },
-  price: { type: Number, required: true },
-  stock: { type: Number, required: true },
-  sold: { type: Number, default: 0 },
-  photoURL: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-});
+const productSchema = new Schema<ProductDocument>(
+  {
+    name: { type: String, trim: true, required: true, maxlength: 32 },
+    description: { type: String, trim: true, required: true, maxlength: 2048 },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    sold: { type: Number, default: 0 },
+    photoURL: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  },
+  { timestamps: true }
+);
 
 productSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateProduct" });
 
