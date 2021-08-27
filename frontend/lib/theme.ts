cb962475c0d91fd8ba6013a9f48dbb61f0fc0bc3
AkashSDas <aakashdas368@gmail.com>
2021-08-27 19:34:18 +0530

    add primary, secondary and text colors

--- frontend/lib/theme.ts
+++ frontend/lib/theme.ts
@@ -2,8 +2,11 @@ import { createTheme } from "@material-ui/core";
 
 const materialUITheme = createTheme({
   palette: {
-    primary: {
-      main: "#0091ff",
+    primary: { main: "#f7f7f7" }, /// white
+    secondary: { main: "#309bff" }, /// blue
+    text: {
+      primary: "#141414", /// darker black
+      secondary: "#242424", /// ligher black
     },
   },
 });
