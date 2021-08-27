63b0692efe6d26fa9743e33e198d98a0d985e0ef
AkashSDas <aakashdas368@gmail.com>
2021-08-27 18:56:50 +0530

    create material ui theme

---
+++ frontend/lib/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@material-ui/core";
+
+const materialUITheme = createTheme({
+  palette: {
+    primary: {
+      main: "#0091ff",
+    },
+  },
+});
+
+export default materialUITheme;
