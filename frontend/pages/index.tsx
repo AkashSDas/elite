a7f93634256081f6211193cf3966137d792898b4
AkashSDas <aakashdas368@gmail.com>
2021-08-28 20:16:11 +0530

    use styled container and navbar in index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,26 @@
+import { Container, makeStyles, Theme } from "@material-ui/core";
+import Navbar from "../components/navbar";
+import materialUITheme from "../lib/theme";
+
+const useStyle = makeStyles((props: Theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: materialUITheme.palette.primary.main,
+    fontFamily: materialUITheme.typography.fontFamily,
+    [props.breakpoints.between("xs", "xl")]: {
+      paddingLeft: "0px",
+      paddingRight: "0px",
+    },
+  },
+}));
+
 function Index() {
+  const classes = useStyle();
+
   return (
-    <main>
-      <h1>Elite</h1>
-    </main>
+    <Container className={classes.root}>
+      <Navbar />
+    </Container>
   );
 }
 
