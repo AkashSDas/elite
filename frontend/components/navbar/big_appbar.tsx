086849f06d8f08211b9da55fea4ce7257df199f5
AkashSDas <aakashdas368@gmail.com>
2021-08-28 23:12:27 +0530

    create big appbar

---
+++ frontend/components/navbar/big_appbar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import SimpleBtn from "../btn/simple_btn";
+
+function BigAppBar({ classes }) {
+  return (
+    <AppBar className={classes.root}>
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.logo}>elite</Typography>
+
+        <Container className={classes.container}>
+          <Typography className={classes.listItemTypography}>User</Typography>
+          <Typography className={classes.listItemTypography}>Admin</Typography>
+          <Typography className={classes.listItemTypography}>Cart</Typography>
+        </Container>
+
+        <Container className={classes.container}>
+          <Typography className={classes.listItemTypography}>Login</Typography>
+          <SimpleBtn text="Sign up" onClick={() => {}} />
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default BigAppBar;
