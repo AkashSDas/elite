7724f8c52b2a42421686fbbf292ef5bfee6fca53
AkashSDas <aakashdas368@gmail.com>
2021-08-29 13:34:37 +0530

    solve toolbar padding issue and main content padding in _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,10 @@
-import { Container, makeStyles, Theme, ThemeProvider } from "@material-ui/core";
+import {
+  Container,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+  Toolbar,
+} from "@material-ui/core";
 import Navbar from "../components/navbar";
 import materialUITheme from "../lib/theme";
 import "../styles/globals.css";
@@ -16,7 +22,33 @@ const useStyle = makeStyles((props: Theme) => ({
 
   /// using this mixins to give height to div above component
   /// so that it will give space between navbar and main content
-  toolbar: props.mixins.toolbar,
+  toolbar: {
+    ...props.mixins.toolbar,
+
+    /// The toolbar with which main content is wrapped is there to
+    /// provide space between navbar and main content. Once the mobile
+    /// screen navbar is used this space is not needed, so therefore
+    /// making the display none
+    [props.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
+
+  main: {
+    /// These paddings are in sync with navbar
+
+    paddingLeft: "32px",
+    paddingRight: "32px",
+
+    [props.breakpoints.down("md")]: {
+      paddingLeft: "8px",
+      paddingRight: "8px",
+    },
+    [props.breakpoints.down("sm")]: {
+      paddingLeft: "0px",
+      paddingRight: "0px",
+    },
+  },
 }));
 
 function MyApp({ Component, pageProps }) {
@@ -26,9 +58,11 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider theme={materialUITheme}>
       <Container className={classes.root}>
         <Navbar />
-        <Container>
+        <Container className={classes.main}>
           <div className={classes.toolbar}></div>
-          <Component {...pageProps} />
+          <Toolbar>
+            <Component {...pageProps} />
+          </Toolbar>
         </Container>
       </Container>
     </ThemeProvider>
