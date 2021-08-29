a17cc588e3acf0929783bdc7326cba85516f0d0c
AkashSDas <aakashdas368@gmail.com>
2021-08-29 13:11:09 +0530

    make index page style as base style

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,29 @@
-import { ThemeProvider } from "@material-ui/core";
+import { Container, makeStyles, Theme, ThemeProvider } from "@material-ui/core";
 import Navbar from "../components/navbar";
 import materialUITheme from "../lib/theme";
 import "../styles/globals.css";
 
+const useStyle = makeStyles((props: Theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: materialUITheme.palette.primary.main,
+    fontFamily: materialUITheme.typography.fontFamily,
+    [props.breakpoints.between("xs", "xl")]: {
+      paddingLeft: "0px",
+      paddingRight: "0px",
+    },
+  },
+}));
+
 function MyApp({ Component, pageProps }) {
+  const classes = useStyle();
+
   return (
     <ThemeProvider theme={materialUITheme}>
-      <Navbar/>
-      <Component {...pageProps} />
+      <Container className={classes.root}>
+        <Navbar />
+        <Component {...pageProps} />
+      </Container>
     </ThemeProvider>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,26 +2,8 @@ import { Container, makeStyles, Theme } from "@material-ui/core";
 import Navbar from "../components/navbar";
 import materialUITheme from "../lib/theme";
 
-const useStyle = makeStyles((props: Theme) => ({
-  root: {
-    height: "100vh",
-    backgroundColor: materialUITheme.palette.primary.main,
-    fontFamily: materialUITheme.typography.fontFamily,
-    [props.breakpoints.between("xs", "xl")]: {
-      paddingLeft: "0px",
-      paddingRight: "0px",
-    },
-  },
-}));
-
 function Index() {
-  const classes = useStyle();
-
-  return (
-    <Container className={classes.root}>
-      <main></main>
-    </Container>
-  );
+  return <main></main>;
 }
 
 export default Index;
