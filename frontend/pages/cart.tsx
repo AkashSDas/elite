fc1b1a616e59b4a28550da0dc220d1b29734bf38
AkashSDas <aakashdas368@gmail.com>
2021-08-31 20:19:26 +0530

    create cart page

---
+++ frontend/pages/cart.tsx
@@ -0,0 +1,32 @@
+import { makeStyles, Theme, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import ProductGrid from "../components/card/product_grid";
+import { loadCart } from "../lib/cart";
+
+const useStyle = makeStyles((theme: Theme) => ({
+  main: {
+    paddingTop: theme.spacing(2),
+    flexGrow: 1,
+  },
+}));
+
+function Cart() {
+  const classes = useStyle();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, []);
+
+  return (
+    <main className={classes.main}>
+      {products.length === 0 ? (
+        <Typography variant="h1">No products in your cart</Typography>
+      ) : (
+        <ProductGrid products={products} />
+      )}
+    </main>
+  );
+}
+
+export default Cart;
