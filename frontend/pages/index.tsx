722cbed74937c5cc5746563d4728c8693413ab43
AkashSDas <aakashdas368@gmail.com>
2021-08-31 19:48:31 +0530

    add content in index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,62 @@
-import { Container, makeStyles, Theme } from "@material-ui/core";
-import Navbar from "../components/navbar";
-import materialUITheme from "../lib/theme";
+import { Grid, makeStyles, Theme } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { fetchFromAPI } from "../lib/api/base";
+import Skeleton from "@material-ui/lab/Skeleton";
+import ProductGridItem from "../components/card/product_card";
+
+const useStyle = makeStyles((theme: Theme) => ({
+  main: {
+    paddingTop: theme.spacing(2),
+    flexGrow: 1,
+  },
+}));
 
 function Index() {
-  return <main></main>;
+  const classes = useStyle();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getProducts = async () => {
+    setLoading(true);
+    const [res, err] = await fetchFromAPI("/product?limit=4", {
+      method: "GET",
+    });
+    setLoading(false);
+    if (res) setProducts(res.data.data.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const loadingJsx = () => (
+    <Grid container justifyContent="center" spacing={2}>
+      {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => (
+        <Grid key={value} item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Skeleton
+            animation="wave"
+            variant="rect"
+            height="200px"
+            width="100%"
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+
+  const productsJsx = () => (
+    <Grid container justifyContent="center" spacing={2}>
+      {products.map((product, key: number) => (
+        <ProductGridItem key={key} product={product} />
+      ))}
+    </Grid>
+  );
+
+  return (
+    <main className={classes.main}>
+      {loading ? loadingJsx() : productsJsx()}
+    </main>
+  );
 }
 
 export default Index;
