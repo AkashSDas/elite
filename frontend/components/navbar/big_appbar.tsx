ea73b77c29bdcb3739d82a3365ff488c53095255
AkashSDas <aakashdas368@gmail.com>
2021-08-31 13:39:44 +0530

    conditional display and logout in BigAppBar

--- frontend/components/navbar/big_appbar.tsx
+++ frontend/components/navbar/big_appbar.tsx
@@ -6,17 +6,52 @@ import PersonIcon from "@material-ui/icons/Person";
 import LaptopWindowsIcon from "@material-ui/icons/LaptopWindows";
 import { PersonAdd, ShoppingCart } from "@material-ui/icons";
 import Link from "next/link";
+import { isAuthenticated, logout } from "../../lib/api/auth";
+import toast from "react-hot-toast";
 
 function BigAppBar({ classes }) {
   const router = useRouter();
 
   /// Nav items
   const navitems = [
-    new NavItemData("User", "/user", <PersonIcon />),
-    new NavItemData("Admin", "/admin", <LaptopWindowsIcon />),
-    new NavItemData("Cart", "/cart", <ShoppingCart />),
-    new NavItemData("Login", "/auth/login", <PersonAdd />),
+    new NavItemData("User", "/user", <PersonIcon />, false, true, true),
+    new NavItemData(
+      "Admin",
+      "/admin",
+      <LaptopWindowsIcon />,
+      false,
+      true,
+      true
+    ),
+    new NavItemData("Cart", "/cart", <ShoppingCart />, false, true, true),
+    new NavItemData("Login", "/auth/login", <PersonAdd />, false, true, false),
+    new NavItemData("Sign up", "/auth/signup", null, true, true, false),
+    new NavItemData("Logout", "/", null, true, true, true),
   ];
+
+  const navItem = (item: NavItemData, key: number) =>
+    item.isBtn ? (
+      item.getSimpleBtn(async () => {
+        if (item.text === "Logout") {
+          await logout(() => toast.success("Successfully logged out"));
+        }
+        router.push(item.route);
+      })
+    ) : (
+      <Link href={item.route} key={key}>
+        <Typography className={classes.listItemTypography}>
+          {item.text}
+        </Typography>
+      </Link>
+    );
+
+  const authCheck = (item: NavItemData, key: number) => {
+    if (item.authCheck) {
+      if (item.displayOnAuth && isAuthenticated()) return navItem(item, key);
+      else if (!item.displayOnAuth && !isAuthenticated())
+        return navItem(item, key);
+    } else return navItem(item, key);
+  };
 
   return (
     <AppBar className={classes.root} elevation={1}>
@@ -24,17 +59,9 @@ function BigAppBar({ classes }) {
         <Typography className={classes.logo}>elite</Typography>
 
         <Container className={classes.container}>
-          {navitems.map((item: NavItemData, key: number) => (
-            <Link href={item.route}>
-              <Typography className={classes.listItemTypography}>
-                {item.text}
-              </Typography>
-            </Link>
-          ))}
-          <SimpleBtn
-            text="Sign up"
-            onClick={() => router.push("/auth/signup")}
-          />
+          {navitems.map((item: NavItemData, key: number) =>
+            authCheck(item, key)
+          )}
         </Container>
       </Toolbar>
     </AppBar>
